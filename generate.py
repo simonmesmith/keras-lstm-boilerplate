d26310ddc15065ca73fb5177e0e178e2b2b3ae77
simonmesmith <simon@simonsmith.ca>
2017-07-25 17:12:23 -0400

    Now saves and loads models

--- generate.py
+++ generate.py
@@ -14,17 +14,18 @@ Here's my understanding of how this works:
 9. Repeat steps 7 and 8
 
 Here are some things I'd like to do:
-1. Experiment with this: (a) try to load a compiled model; (b) if the model doesn't exist, create it and save it
-2. Experiment with this: Instead of generating a random seed, explicitly provide the seed or seeds to the application
+1. Experiment with this: Instead of generating a random seed, explicitly provide the seed or seeds to the application
 
 '''
 
 from __future__ import print_function
-from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.layers import LSTM
+from keras.models import load_model
+from keras.models import Sequential
 from keras.optimizers import RMSprop
 from keras.utils.data_utils import get_file
+import os.path
 import numpy as np
 import random
 import sys
@@ -41,51 +42,68 @@ char_indices = dict((c, i) for i, c in enumerate(chars))
 # Create another character index array that's the inverse of char_indices (e.g. ['0': a, '1': b]).
 indices_char = dict((i, c) for i, c in enumerate(chars))
 
-# Cut the text in "semi-redundant sequences of maxlen characters," according to the author.
-maxlen = 40 # Maximum length of a "sentence" (string)
-step = 3 # Steps between numbers (e.g. 0, 3, 6, 9)
-sentences = [] # Variable to hold "sentences"
-next_chars = [] # Variable to hold the next character after each sentence
-for i in range(0, len(text) - maxlen, step): # Loop from 0 to the text length minus the maximum sentence length in increments defined by the step variable
-    sentences.append(text[i: i + maxlen]) # Append a sentence using the index as the starting point
-    next_chars.append(text[i + maxlen]) # Append the first character that comes after the sentence
+# Set the maximum length of a "sentence" (used throughout the code).
+maxlen = 40
 
-# Vectorize the sentences for analysis.
-# To learn: Who do we use the boolean type for X and y variables below?
-# To learn: Why do we set X[] and y[] to equal 1? What's the significance of that?
-X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool) # Create an empty (?) array for training data with the shape specified and the data type boolean
-y = np.zeros((len(sentences), len(chars)), dtype=np.bool) # Create an empty (?) array for target data (what we want to predict) with the shape specified and the data type boolean
-for i, sentence in enumerate(sentences): # For each sentence in sentences (i = index, sentence = sentence)
-    for t, char in enumerate(sentence): # For each character in the sentence (t = index, char = character)
-        X[i, t, char_indices[char]] = 1 # Add to training data array (sentence index, sentence character index, index of character in char_indices array)
-        y[i, char_indices[next_chars[i]]] = 1 # Add to target data array (sentence index, index of character in char_indices for the character that comes after the sentence)
+# Set a variable to hold the compiled model's filename.
+modelFileName = 'model.h5'
 
-# Create neural network.
-# To learn: What are "units" in this context?
-# To learn: What does the softmax activation function do? What is "softmax?"
-model = Sequential() # Model is sequential
-model.add(LSTM(128, input_shape=(maxlen, len(chars)))) # Add an LSTM with 128 units and an input shape of maxlen (sentence maximum length) and the number of unique characters in the text (len(chars))
-model.add(Dense(len(chars))) # Add a Dense layer with as many units as there are unique characters in the text (len(chars)); not that it infers the input shape from the previous layer
-model.add(Activation('softmax')) # Add the output layer with a softmax activation function
+# If we already have a compiled model...
+if os.path.exists(modelFileName):
 
-# Set optimizer variable.
-# To learn: What do optimizers do in the context of machine learning?
-# To learn: What is learning rate? What does this do?
-optimizer = RMSprop(lr=0.01) # RMSprop is apparently a particularly good optimizer for recurrent neural networks; lr is the learning rate
+    # Set a variable to hold the compiled model.
+    model = load_model(modelFileName)
 
-# Compile model.
-# To learn: How do loss functions work? Why are there different ones?
-# To learn: What is the "categorical_crossentropy" loss function? Why do we use it here?
-model.compile(loss='categorical_crossentropy', optimizer=optimizer) # Compile with loss function and optimizer;
+# Else create, compile and save the model.
+else:
 
-# Train model. Note that in the author's original version, this is inside the loop below, and sequentially trained with each iteration. I believe the purpose of that
-# was simply to show how the model improves with each iteration, and that this isn't necessary for a more production-oriented approach.
-model.fit( # Train the model
-    X, # Training data (populated above)
-    y, # Target data (what to predict; populated above)
-    batch_size=128, # Number of samples per gradient update
-    epochs=20 # Number of times to iterate over the training data in each iteration
-)
+    # Cut the text in "semi-redundant sequences of maxlen characters," according to the author.
+    step = 3 # Steps between numbers (e.g. 0, 3, 6, 9)
+    sentences = [] # Variable to hold "sentences"
+    next_chars = [] # Variable to hold the next character after each sentence
+    for i in range(0, len(text) - maxlen, step): # Loop from 0 to the text length minus the maximum sentence length in increments defined by the step variable
+        sentences.append(text[i: i + maxlen]) # Append a sentence using the index as the starting point
+        next_chars.append(text[i + maxlen]) # Append the first character that comes after the sentence
+
+    # Vectorize the sentences for analysis.
+    # To learn: Who do we use the boolean type for X and y variables below?
+    # To learn: Why do we set X[] and y[] to equal 1? What's the significance of that?
+    X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool) # Create an empty (?) array for training data with the shape specified and the data type boolean
+    y = np.zeros((len(sentences), len(chars)), dtype=np.bool) # Create an empty (?) array for target data (what we want to predict) with the shape specified and the data type boolean
+    for i, sentence in enumerate(sentences): # For each sentence in sentences (i = index, sentence = sentence)
+        for t, char in enumerate(sentence): # For each character in the sentence (t = index, char = character)
+            X[i, t, char_indices[char]] = 1 # Add to training data array (sentence index, sentence character index, index of character in char_indices array)
+            y[i, char_indices[next_chars[i]]] = 1 # Add to target data array (sentence index, index of character in char_indices for the character that comes after the sentence)
+
+    # Create neural network.
+    # To learn: What are "units" in this context?
+    # To learn: What does the softmax activation function do? What is "softmax?"
+    model = Sequential() # Model is sequential
+    model.add(LSTM(128, input_shape=(maxlen, len(chars)))) # Add an LSTM with 128 units and an input shape of maxlen (sentence maximum length) and the number of unique characters in the text (len(chars))
+    model.add(Dense(len(chars))) # Add a Dense layer with as many units as there are unique characters in the text (len(chars)); not that it infers the input shape from the previous layer
+    model.add(Activation('softmax')) # Add the output layer with a softmax activation function
+
+    # Set optimizer variable.
+    # To learn: What do optimizers do in the context of machine learning?
+    # To learn: What is learning rate? What does this do?
+    optimizer = RMSprop(lr=0.01) # RMSprop is apparently a particularly good optimizer for recurrent neural networks; lr is the learning rate
+
+    # Compile model.
+    # To learn: How do loss functions work? Why are there different ones?
+    # To learn: What is the "categorical_crossentropy" loss function? Why do we use it here?
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer) # Compile with loss function and optimizer;
+
+    # Train model. Note that in the author's original version, this is inside the loop below, and sequentially trained with each iteration. I believe the purpose of that
+    # was simply to show how the model improves with each iteration, and that this isn't necessary for a more production-oriented approach.
+    model.fit( # Train the model
+        X, # Training data (populated above)
+        y, # Target data (what to predict; populated above)
+        batch_size=128, # Number of samples per gradient update
+        epochs=20 # Number of times to iterate over the training data in each iteration
+    )
+
+    # Save the model with the specified file name.
+    model.save(modelFileName)
 
 # Create a function that "samples an index from a probability array," according to the author.
 # To learn: What does this do? Why do we need it?
@@ -97,8 +115,8 @@ def sample(preds, temperature=1.0):
     probas = np.random.multinomial(1, preds, 1) # Sets a variable to sample from a multinomial distribution
     return np.argmax(probas) # "Returns the indices of the maximum values along an axis"; don't understand this fully
 
-# Train the model and output generated text after each iteration.
-for iteration in range(1, 10): # For iterations in the range specified...
+# Make a batch of new drug names
+for iteration in range(1, 2): # For iterations in the range specified...
 
     # Print header.
     print() # Prints blank
