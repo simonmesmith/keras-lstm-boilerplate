5fffee6f47b425517dc34938a11ea084145ddc1d
simonmesmith <simon@simonsmith.ca>
2017-08-16 14:23:02 -0400

    Big improvements to generation
    Removed iteration and now just generate text of specified output length (number of characters). Cleaner code and better outputs.

--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ import numpy as np
 import random
 import sys
 
-def write(inputFilename, outputFilename, scanLength=5, outputLength=10, numberOfOutputs=10, creativity=0.2):
+def write(inputFilename, outputFilename, scanLength, outputLength, creativity=0.2):
 
     # Set key variables.
     text = helper.getText(inputFilename)
@@ -25,36 +25,32 @@ def write(inputFilename, outputFilename, scanLength=5, outputLength=10, numberOf
         probabilities = np.random.multinomial(1, predictions, 1) # Sets a variable to sample from a multinomial distribution
         return np.argmax(probabilities) # "Returns the indices of the maximum values along an axis"; don't understand this fully
 
-    # Make a batch of new outputs
-    outputs = []
-    for iteration in range(1, numberOfOutputs): # For iterations in the range specified...
+    # Create seed text and a generated variable to hold generated text.
+    start_index = random.randint(0, len(text) - scanLength - 1) # Sets the start index for the seed to a random start point with sufficient length remaining for an appropriate-length seed
+    seed = text[start_index: start_index + scanLength] # Sets the seed, a string of scanLength length from the text that starts from the random start index variable
+    generated = '' # Sets a generated variable to hold generated text
 
-        # Create a generated variable and start it off with seed text.
-        start_index = random.randint(0, len(text) - scanLength - 1) # Sets the start index for the seed to a random start point with sufficient length remaining for an appropriate-length seed
-        seed = text[start_index: start_index + scanLength] # Gets a string of scanLength length from the text using the random start index variable
-        generated = seed # Sets a generated variable using the seed as the starting text
+    # Generate text for that's the output length (number of characters) specified
+    for i in range(outputLength): # Create an output of outputLength length by looping that many times and adding a character each time
 
-        # Loop and generate text.
-        for i in range(outputLength): # Create an output of outputLength length by looping that many times and adding a character each time
+        # Vectorize the generated text.
+        # To learn: Why do we set x[] = 1? Why does that 1 have a period following it? Assumption: one-hot encoding.
+        x = np.zeros((1, scanLength, len(chars))) # Set a variable to an array of the specified shape that will hold the generated text in vectorized form
+        for t, char in enumerate(seed): # Loop through each character in the seed, with t as the character index and char as the character
+            x[0, t, char_indices[char]] = 1. # Append the character index from the seed (t) and the character index of the character to the variable x
 
-            # Vectorize the generated text.
-            # To learn: Why do we set x[] = 1? Why does that 1 have a period following it? Assumption: one-hot encoding.
-            x = np.zeros((1, scanLength, len(chars))) # Set a variable to an array of the specified shape that will hold the generated text in vectorized form
-            for t, char in enumerate(generated): # Loop through each character in the generated text, with t as the character index and char as the character
-                x[0, t, char_indices[char]] = 1. # Append the character index from the generated text (t) and the character index of the character to the variable x
+        # Predict the next character for the seed text.
+        predictions = model.predict(x, verbose=0)[0] # Set a variable to hold a prediction; x is the input data, verbosity of 0 to suppress logging
+        next_index = sample(predictions, creativity) # Get the predicted next_index value from an array of predictions using the specified level of creativity
+        next_char = indices_char[next_index] # Set a next_char variable to an actual character using the inverse character indices variable
 
-            # Predict the next character for the generated text.
-            predictions = model.predict(x, verbose=0)[0] # Set a variable to hold a prediction; x is the input data, verbosity of 0 to suppress logging
-            next_index = sample(predictions, creativity) # Get the predicted next_index value from an array of predictions using the specified level of creativity
-            next_char = indices_char[next_index] # Set a next_char variable to an actual character using the inverse character indices variable
+        # Add character to generated text.
+        generated = generated + next_char
 
-            # Update generated text. Note that we overwrite the text moving from left to right, therefore overwriting the initial seed text with all generated text.
-            generated = generated[1:] + next_char # Remove first letter of seed and add next_char at the end; this is then used in the next loop to predict the next character
-
-        # Append the generated text to the output
-        outputs.append(generated)
+        # Create the seed for the next loop.
+        seed = generated[-scanLength:]
 
     # Save outputs to file.
     text_file = open('outputs/' + outputFilename, "w")
-    text_file.write('\n'.join(outputs))
+    text_file.write(generated)
     text_file.close()
--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import generator
 import helper
 
 # Generate.
-generator.write(inputFilename='drug-brand-names.txt', outputFilename='new-drug-brand-names.txt', scanLength=10, outputLength=10, numberOfOutputs=200, creativity=0.5)
+generator.write(inputFilename='drug-brand-names.txt', outputFilename='new-drug-brand-names.txt', scanLength=10, outputLength=500, creativity=0.5)
 
 # Clean up generated output.
-helper.cleanOutput(outputFilename='new-drug-brand-names.txt', minOutputLength=6)
+helper.cleanOutput(outputFilename='new-drug-brand-names.txt', minOutputLength=3)
