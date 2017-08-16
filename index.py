4c13645ac35e50ba3a16da7b95e8d363f315526a
simonmesmith <simon@simonsmith.ca>
2017-08-16 15:59:16 -0400

    Going to try generating new tweets based on my historical tweets

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import generator
 import helper
 
 # Generate.
-generator.write(inputFilename='drug-brand-names.txt', outputFilename='new-drug-brand-names.txt', scanLength=10, outputLength=500, creativity=0.5)
+generator.write(inputFilename='simon-smith-tweets.txt', outputFilename='new-simon-smith-tweets.txt', scanLength=10, outputLength=500, creativity=0.1)
 
 # Clean up generated output.
-helper.cleanOutput(outputFilename='new-drug-brand-names.txt', minOutputLength=3)
+helper.cleanOutput(outputFilename='new-simon-smith-tweets.txt', minOutputLength=3)
